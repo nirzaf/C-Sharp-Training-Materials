928c393c2d051f1bd6a040f00b15dd90273c7c97
nirzaf <mfmfazrin1986@gmail.com>
2022-05-17 20:40:29 +0530

    var names = new List<string> { "<name>", "Ana", "Felipe" }; foreach (var name in names) {     Console.WriteLine($"Hello {name.ToUpper()}!"); }

--- Array-Lists/Program.cs
+++ Array-Lists/Program.cs
@@ -1,5 +1,9 @@
-﻿var names = new List<string> { "<name>", "Ana", "Felipe" };
+﻿
+//List of Names 
+
+var names = new List<string> { "<name>", "Ana", "Felipe" };
 foreach (var name in names)
 {
     Console.WriteLine($"Hello {name.ToUpper()}!");
 }
+
