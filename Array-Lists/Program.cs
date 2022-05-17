ed29cae511103d5d463a4ed9e6b9da55e829e343
nirzaf <mfmfazrin1986@gmail.com>
2022-05-17 20:24:26 +0530

    Commit the changes

--- Array-Lists/Program.cs
+++ Array-Lists/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-var names = new List<string> { "<name>", "Ana", "Felipe" };
+﻿var names = new List<string> { "<name>", "Ana", "Felipe" };
 foreach (var name in names)
 {
     Console.WriteLine($"Hello {name.ToUpper()}!");
