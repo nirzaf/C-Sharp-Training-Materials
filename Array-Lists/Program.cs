59eb4f5896a25d0c3fe044f541d97ad949330106
nirzaf <mfmfazrin1986@gmail.com>
2022-05-17 20:21:34 +0530

    first commit

---
+++ Array-Lists/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+
+var names = new List<string> { "<name>", "Ana", "Felipe" };
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
---
+++ Training Scripts/.history/ArraysList_20220517201443.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+
+var names = new List<string> { "<name>", "Ana", "Felipe" };
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+}
