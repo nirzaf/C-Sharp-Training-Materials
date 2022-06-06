f5cf6e3fd3c06032f0bebe1dfa89ac7efb6b9d02
Fazrin <mfmfazrin1986@gmail.com>
2022-06-06 15:50:59 +0530

    commit

---
+++ Palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine(getMin("))((  )"));
+
+static int DoIt(int x)
+{
+    x += 1;
+    Console.WriteLine(x);
+    return x;
+}
+
+//Get the number parenthesis required to to have balanced parenthesis
+static int getMin(string s)
+{
+    int OpenBrancket = 0;
+    int CloseBrancket = 0;
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i] == '(')
+        {
+            OpenBrancket++;
+        }
+        else if (s[i] == ')')
+        {
+            CloseBrancket++;
+        }
+    }
+
+    return OpenBrancket >= CloseBrancket ? OpenBrancket - CloseBrancket : CloseBrancket - OpenBrancket;
+}
