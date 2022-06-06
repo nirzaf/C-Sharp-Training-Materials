00a412e9f69c2a903c8d6f9d51df5d175dca1edf
Fazrin <mfmfazrin1986@gmail.com>
2022-06-06 16:04:45 +0530

    Console.WriteLine("Count : " + count);     return maxVal - minVal - count + 1;

---
+++ ArraySequence/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] n = { 3, 5, 11, 2 };
+
+Console.WriteLine(FindIt(n));
+
+static int FindIt(int[] num)
+{
+    Array.Sort(num);
+    int maxVal = num[num.Length - 1];
+    Console.WriteLine("Max Value : " + maxVal);
+    int minVal = num[0];
+    Console.WriteLine("Min Value : " + minVal);
+    int count = num.Count();
+    Console.WriteLine("Count : " + count);
+    return maxVal - minVal - count + 1;
+}
