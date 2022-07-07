1384f60b954070b58f43a6eea767e2470f3c42d1
Fazrin <mfmfazrin1986@gmail.com>
2022-07-07 16:47:08 +0300

    class updated

---
+++ Palindrome/Ball.cs
@@ -0,0 +1,33 @@
+﻿namespace Palindrome;
+
+public class Ball
+{
+    private string _color;
+    private int _size;
+    private int _weight;
+    private int _speed;
+    private int _position;
+    private int _direction;
+    
+    public Ball(string color, int size, int weight, int speed, int position, int direction)
+    {
+        _color = color;
+        _size = size;
+        _weight = weight;
+        _speed = speed;
+        _position = position;
+        _direction = direction;
+    }
+    
+    public void PlayBall()
+    {
+        if (_direction == 1)
+        {
+            _position += _speed;
+        }
+        else
+        {
+            _position -= _speed;
+        }
+    }
+}
