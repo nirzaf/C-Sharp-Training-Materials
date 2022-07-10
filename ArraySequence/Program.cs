6529f665acc2eca9ad5bc24c30565ee37abf8703
Fazrin <mfmfazrin1986@gmail.com>
2022-07-10 15:46:52 +0300

    pushed by mfmfazrin1986@gmail.com at Sunday, July 10, 2022, 06:16:52 PM

--- ArraySequence/Program.cs
+++ ArraySequence/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine(FindIt(n));
 static int FindIt(int[] num)
 {
     Array.Sort(num);
-    int maxVal = num[num.Length - 1];
+    int maxVal = num[^1];
     Console.WriteLine("Max Value : " + maxVal);
     int minVal = num[0];
     Console.WriteLine("Min Value : " + minVal);
