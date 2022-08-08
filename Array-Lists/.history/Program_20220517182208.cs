a5f79ae02a466a5727dbbc7724021e985468c43a
Fazrin <mfmfazrin1986@gmail.com>
2022-08-08 21:29:46 +0300

    commit

---
+++ Array-Lists/.history/Program_20220517182208.cs
@@ -0,0 +1,31 @@
+﻿
+//List of Names 
+
+//var names = new List<string> { "<name>", "Ana", "Felipe" };
+//foreach (var name in names)
+//{
+//    Console.WriteLine($"Hello {name.ToUpper()}!");
+//}
+
+
+//var namesList = new List<string> { "Sophia", "Ana", "Jayme", "Bill" };
+//string nameIndex = "Ana";
+//var index = namesList.IndexOf(nameIndex);
+//Console.WriteLine($"Found {nameIndex} at {index}");
+
+
+var names = new List<string> { "Sophia", "Ana", "Jayme", "Bill" };
+Console.WriteLine("Pre Sorting:");
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+
+names.Sort();
+
+Console.WriteLine();
+Console.WriteLine("Post Sorting:");
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
--- Array-Lists/Program.cs
+++ Array-Lists/Program.cs
@@ -1,19 +1,4 @@
 ﻿
-//List of Names 
-
-//var names = new List<string> { "<name>", "Ana", "Felipe" };
-//foreach (var name in names)
-//{
-//    Console.WriteLine($"Hello {name.ToUpper()}!");
-//}
-
-
-//var namesList = new List<string> { "Sophia", "Ana", "Jayme", "Bill" };
-//string nameIndex = "Ana";
-//var index = namesList.IndexOf(nameIndex);
-//Console.WriteLine($"Found {nameIndex} at {index}");
-
-
 var names = new List<string> { "Sophia", "Ana", "Jayme", "Bill" };
 Console.WriteLine("Pre Sorting:");
 foreach (var name in names)
